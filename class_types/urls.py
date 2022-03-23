3084153499c235e9390bb7aa0d6f65611b62a3bb
Alex Silva <alesilvasp@gmail.com>
2022-03-23 11:23:24 -0300

    fix: change urls endpoints and added tests report

--- class_types/urls.py
+++ class_types/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import TypeCreateView, TypeUpdateView
 
 urlpatterns = [
-    path('classes/<class_id>/types', TypeCreateView.as_view()),
-    path('classes/types/<type_id>', TypeUpdateView.as_view())
+    path('classes/<class_id>/types/', TypeCreateView.as_view()),
+    path('classes/types/<type_id>/', TypeUpdateView.as_view())
 ]
